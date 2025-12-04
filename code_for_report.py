769f24098aacc87db6688c480c8b44b0c16113d6
Nicholas Thole <nicholas.thole@epfl.ch>
2025-12-04 19:49:20 +0100

    add function for visib graph in code_for_report

--- code_for_report.py
+++ code_for_report.py
@@ -1,5 +1,8 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+from modified_astar import PathPlanner
+from visualizer import Visualizer
 
 def filter_color(image):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -57,4 +60,62 @@ def draw_contours(image, scaled_contours, all_vertices):
     cv2.drawContours(output, scaled_contours, -1, (0, 0, 255), 6)
 
     return output
+
+
+def visualize_visibility_graph(start, goal, obstacles, image_path):
+    """
+    Visualize the visibility graph on a calibrated frame image.
+    
+    Args:
+        start: np.array [x, y] start position
+        goal: np.array [x, y] goal position
+        obstacles: list of np.array polygons
+        image_path: str, path to the background image
+    
+    Returns:
+        matplotlib figure object
+    """
+    
+    # Build nodes list
+    nodes = [start, goal]
+    for poly in obstacles:
+        for v in poly:
+            nodes.append(np.array(v))
+    
+    # Create planner and build visibility graph
+    planner = PathPlanner()
+    planner.graph = planner._build_visibility_graph(nodes, obstacles)
+    
+    # Load the calibrated frame image
+    frame = cv2.imread(image_path)
+    
+    # Draw obstacles on the frame
+    vis = Visualizer()
+    frame = vis.draw_obstacles(frame, obstacles)
+    
+    # Draw all visibility graph edges
+    for i, connections in planner.graph.items():
+        for j, dist in connections:
+            if i < j:  # Draw each edge only once
+                pt1 = (int(nodes[i][0]), int(nodes[i][1]))
+                pt2 = (int(nodes[j][0]), int(nodes[j][1]))
+                cv2.line(frame, pt1, pt2, (0, 0, 0), 2)  # Black lines, thickness 2
+    
+    # Draw all nodes
+    for i, node in enumerate(nodes):
+        if i == 0:  # Start
+            cv2.circle(frame, (int(node[0]), int(node[1])), 8, (255, 0, 0), -1)  # Blue
+        elif i == 1:  # Goal
+            cv2.circle(frame, (int(node[0]), int(node[1])), 8, (0, 255, 0), -1)  # Green
+        else:  # Obstacle vertices
+            cv2.circle(frame, (int(node[0]), int(node[1])), 5, (255, 255, 255), -1)  # White
+    
+    # Display the result
+    fig = plt.figure(figsize=(9, 6))
+    plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    plt.title('Visibility Graph')
+    plt.axis('off')
+    plt.tight_layout()
+    
+    return fig
 
